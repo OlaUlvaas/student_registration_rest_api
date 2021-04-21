d9a869ed45481ff07662f360f8dbcc15d39b84b4
olaulvas <ola.ulvaas@gmail.com>
2021-04-21 09:20:19 +0200

    Initial commit

---
+++ src/test/java/se/lexicon/ola/registration/StudentRegistrationRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.ola.registration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentRegistrationRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
